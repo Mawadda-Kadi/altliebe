5629c90de989227f37cc9d1a0018260a53077bab
Mawadda Kadi <mawadda.kadi@gmail.com>
2024-03-03 06:46:48 +0000

    create: superuser & secret key

--- altliebe/settings.py
+++ altliebe/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sw)%p)7#-5)4(w&q$^exl&--c9$)(jsk&h2x*=hma0&&#sgez1'
+#SECRET_KEY = 'django-insecure-sw)%p)7#-5)4(w&q$^exl&--c9$)(jsk&h2x*=hma0&&#sgez1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
