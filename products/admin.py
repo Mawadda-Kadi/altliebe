76483f4441f5c9dbbb8b2f79e15fbdd5e126069c
Mawadda Kadi <mawadda.kadi@gmail.com>
2024-03-03 07:34:42 +0000

    add: products admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_at', 'price', 'availability')
+    list_filter = ('status', 'created_at', 'availability')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    ordering = ('status', 'created_at')
